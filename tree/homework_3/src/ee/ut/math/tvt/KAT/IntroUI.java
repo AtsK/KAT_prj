a309bd58921cbe34ea4c71de8c13a97861b5c08a
Thomas Toodo <thomastoodo@msn.com>
2014-10-08 14:30:46 +0300

    IntroUI

--- tree/homework_3/src/ee/ut/math/tvt/KAT/IntroUI.java
+++ tree/homework_3/src/ee/ut/math/tvt/KAT/IntroUI.java
@@ -1,5 +1,40 @@
 package ee.ut.math.tvt.KAT;
 
-public class IntroUI {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
+public class IntroUI {
+	
+	
+	public static void main(String s[]) {
+		JFrame frame = new JFrame("IntroUI");
+		frame.setSize(500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel nimi = new JLabel("KAT");
+		JLabel liider = new JLabel("puudub");
+		JLabel email = new JLabel("Thomastoodo@msn.com");
+		
+		
+		JPanel listPane = new JPanel();
+		listPane.setLayout(new BoxLayout(listPane, BoxLayout.PAGE_AXIS));
+		
+		
+		listPane.add(nimi);
+		listPane.add(liider);
+		listPane.add(email);
+		
+		listPane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		Container container = new JPanel();
+		container.add(listPane, BorderLayout.CENTER);
+		
+	
+		
+
+		frame.setVisible(true);
+	}
+	
+	
+	
+	
 }
